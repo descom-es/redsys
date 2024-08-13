f48d8c989a41f32d325c3b9e2b8a47eb971caafb
Cesar <cesargb@gmail.com>
2024-08-13 14:27:12 +0200

    add request laravel

--- src/Payments/InSite/InSite.php
+++ src/Payments/InSite/InSite.php
@@ -5,6 +5,7 @@ namespace Descom\Redsys\Payments\InSite;
 use Descom\Redsys\Environments\Environment;
 use Descom\Redsys\Merchants\Merchant;
 use Descom\Redsys\Response;
+use Illuminate\Http\Request;
 
 final class InSite
 {
@@ -18,7 +19,7 @@ final class InSite
             ->configuration($cardToken, $urlNotification);
     }
 
-    public function process(int|string $orderId, int|float $amount, string $cardToken, string $urlNotification, array $screen, array $em3dSecure, ?HttpRequest $request = null): Response
+    public function process(int|string $orderId, int|float $amount, string $cardToken, string $urlNotification, array $screen, array $em3dSecure, ?Request $request = null): Response
     {
         return (new Emv3DsAuthProcessRequest($this->environment, $this->merchant, $orderId, $amount))
             ->process($cardToken, $urlNotification, $screen, $em3dSecure, $request);
